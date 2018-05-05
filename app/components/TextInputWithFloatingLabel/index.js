789c894caa6e07124dddd707c6edf4d3b24bd5d6
Jojo Narte <Jojo@dnamicro.com>
2018-05-05 14:32:48 +0800

    custom text input component with extra features

---
+++ app/components/TextInputWithFloatingLabel/index.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { TextInput, Text, View } from 'react-native';
+import ViewPropTypes from 'ViewPropTypes';
+import PropTypes from 'prop-types';
+import styles from './styles';
+
+type Props = {
+	containerStyle: mixed,
+	textInputStyle: mixed,
+	labelStyle: mixed,
+	hint: string,
+}
+
+export default class TextInputWithFloatingLabel extends Component {
+	static propTypes = {
+		containerStyle: ViewPropTypes.style,
+		textInputStyle: ViewPropTypes.style,
+		labelStyle: ViewPropTypes.style,
+		hint: PropTypes.string,
+		placeholder: PropTypes.string
+	};
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			isErrorRevealed: false
+		}
+	}
+
+	render() {
+		return (
+			<View style={[styles.container, this.props.containerStyle]}>
+				<Text style={[styles.textInputLabel, this.props.labelStyle]}>
+					{this.props.hint}
+				</Text>
+				<TextInput
+			        style={[styles.textInputField, this.props.textInputStyle]}
+			        onChangeText={(text) => this.setState({text})}
+		        	value={this.state.text}
+		        	placeholder={this.props.placeholder}
+		      />
+			</View>
+			
+		);
+	}
+}
---
+++ app/components/TextInputWithFloatingLabel/styles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+	container: {},
+	textInputLabel: {
+		fontSize: 12,
+		marginBottom: 13,
+	},
+	textInputField: {
+		height: 50,
+		borderColor: '#DAE0E4',
+		backgroundColor: 'white',
+		borderWidth: 1,
+		borderRadius: 4,
+		paddingLeft: 10,
+		paddingRight: 10
+	}
+});

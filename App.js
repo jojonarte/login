35faaf485151bcbd717f6d253fa177c6e81c8eec
Jojo Narte <Jojo@dnamicro.com>
2018-05-05 14:31:47 +0800

    validation and App base

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, AppRegistry } from 'react-native';
+import LoginScreen from './app/screens/LoginScreen';
 
 export default class App extends React.Component {
+ 
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <LoginScreen />
     );
   }
 }
@@ -18,4 +19,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+});  

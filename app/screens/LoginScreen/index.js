2b0cf5d7304550fe74d6d912f7b8ba8e2795d5bd
Jojo Narte <Jojo@dnamicro.com>
2018-05-07 11:51:16 +0800

    tried fixing statusbar but it seems to be a bug with RN?

--- app/screens/LoginScreen/index.js
+++ app/screens/LoginScreen/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StatusBar, KeyboardAvoidingView } from 'react-native';
+import { KeyboardAvoidingView } from 'react-native';
 
 import { LoginForm } from '../../components/LoginForm';
 import { Container } from '../../components/Container';
@@ -7,7 +7,7 @@ import { Logo } from '../../components/Logo';
 
 export default () => (
 	<Container>
-		<KeyboardAvoidingView behavior="padding" style={{width: '100%', alignItems: 'center' }}>
+      	<KeyboardAvoidingView behavior="padding" style={{width: '100%', alignItems: 'center' }}>
 	      	<Logo />
 		    <LoginForm />
 	    </KeyboardAvoidingView>
